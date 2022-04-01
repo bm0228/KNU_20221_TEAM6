47f91d8cfed55ac2edd0938f44871d9e6dd3e8ba
bm0228 <bm0228@naver.com>
2022-04-02 00:43:31 +0900

    애플/오렌지 버전으로 바꿈 (이미지 분류 모델) teachablemachine버전

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,42 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// import 'package:camera/camera.dart';
+// import 'home.dart';
+
+// List<CameraDescription> cameras;
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   cameras = await availableCameras();
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: '인증',
+//       theme: ThemeData(
+//         brightness: Brightness.light,
+//         primaryColor: Colors.blue
+//       ),
+//       home: home(),
+//     );
+//   }
+// }
+
 import 'package:camera/camera.dart';
-import 'home.dart';
+import 'package:flutter/material.dart';
+import 'screens/home_page.dart';
 
 List<CameraDescription> cameras;
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras();
-  runApp(MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyHomePage(
+      cameras,
+    ),
+  ));
 }
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '인증',
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.blue
-      ),
-      home: home(),
-    );
-  }
-}
-

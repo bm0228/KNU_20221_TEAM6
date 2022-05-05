d9ae4a94363afd5d97686409a8f99562d8c98065
kongsh <33220434+kongsh@users.noreply.github.com>
2022-05-06 01:10:14 +0900

    ui update
    ui branch를 가져옴

--- flutter/detect/lib/landmark.dart
+++ flutter/detect/lib/landmark.dart
@@ -5,3 +5,9 @@ List<Map> landmark = [
   {'name': 'north', 'latitude': 35.8919, 'longitude': 128.609952},
   {'name': 'front', 'latitude': 35.88517, 'longitude': 128.61447},
 ];
+
+var nameList = ['북문', '정문'];
+
+var imageList = ['image/north/1.PNG', 'image/front/1.PNG'];
+
+var description = ['북문입니다', '정문입니다'];
